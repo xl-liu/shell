3c10aedc275af6f528ee44adea1aea39750a4ae7
Nicola <liuxueliang1984@gmail.com>
2015-01-07 14:24:30 +0800

    added insertLog.sh

---
+++ insertLog.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/sh
+
+IFS=$'\n'
+entries=$(awk -F# '{print "\""$3"\""}' < log.entries);
+
+for entry in ${entries}
+do 
+  echo "insert into logentry (content) values ("$entry");"
+done | mysql -uroot -proot -A test;
+
+# select count(*) as Frequency, content from logentry group by content order by Frequency;
